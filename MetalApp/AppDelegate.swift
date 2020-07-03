3b8b8ccc12cb00039683dba9d492e896f3d29255
konglee <konglee873@gmail.com>
2020-07-04 01:18:03 +0800

    Initial Commit

---
+++ MetalApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MetalApp
+//
+//  Created by konglee on 2020/7/4.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MetalApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MetalApp
+//
+//  Created by konglee on 2020/7/4.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
